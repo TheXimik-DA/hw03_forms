91e2bb1f0005aad0f0a66776437671a7597f4a0e
Gadzhi Shakhbanov <mister.tool@mail.ru>
2023-01-10 20:20:10 +0300

    11

--- yatube/users/urls.py
+++ yatube/users/urls.py
@@ -1,10 +1,6 @@
 from django.contrib.auth.views import (LoginView, LogoutView,
-                                       PasswordChangeDoneView,
-                                       PasswordChangeView,
-                                       PasswordResetCompleteView,
-                                       PasswordResetConfirmView,
-                                       PasswordResetDoneView,
-                                       PasswordResetView)
+    PasswordChangeDoneView, PasswordChangeView, PasswordResetCompleteView,
+    PasswordResetConfirmView, PasswordResetDoneView, PasswordResetView)
 from django.urls import path
 
 from . import views
--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -40,7 +40,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        "DIRS": [TEMPLATES_DIR],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
