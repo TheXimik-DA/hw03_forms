66b1c4fae49f4e29661c216085cf9d9e3bb928e6
Gadzhi Shakhbanov <mister.tool@mail.ru>
2023-01-11 23:41:11 +0300

    11

--- yatube/users/urls.py
+++ yatube/users/urls.py
@@ -8,18 +8,22 @@ from django.contrib.auth.views import PasswordResetDoneView
 from django.contrib.auth.views import PasswordResetView
 from django.urls import path
 
-from . import views
+from users.views import SignUp
 
 app_name = 'users'
 
 urlpatterns = [
-    path('signup/', views.SignUp.as_view(), name='signup'),
-    path('logout/',
-         LogoutView.as_view(template_name='users/logged_out.html'),
-         name='logout'),
+    path('signup/', SignUp.as_view(), name='signup'),
+    path(
+        'logout/',
+        LogoutView.as_view(
+            template_name='users/logged_out.html'),
+        name='logout',
+    ),
     path(
         'login/',
-        LoginView.as_view(template_name='users/login.html'),
+        LoginView.as_view(
+            template_name='users/login.html'),
         name='login',
     ),
     path(
