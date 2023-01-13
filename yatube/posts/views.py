87308247f5855b6e7f93b128d5dfbc491f3ac2bc
Gadzhi Shakhbanov <mister.tool@mail.ru>
2023-01-13 18:58:01 +0300

    11

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404, redirect, render
+
 from posts.forms import PostForm
 from posts.models import Group, Post, User
 
